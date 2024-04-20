9a9331137786b1cbab2ad4bcc290e44d3322ab21
Shane <143796207+Shane0821@users.noreply.github.com>
2024-04-20 10:48:26 +0800

    upd producer and consumer

--- service/data_processor.py
+++ service/data_processor.py
@@ -1,18 +1,51 @@
 from kafka import KafkaConsumer
 from kafka.errors import KafkaError
+from kafka.admin import KafkaAdminClient, NewTopic
 import json
+import threading
 
-comsumer = KafkaConsumer(
-    'real-time-stock', # topic
-    client_id='real-time-stock-consumer-1',
-    group_id='real-time-stock-group',
-    bootstrap_servers=['localhost:9092'],
-    value_deserializer=lambda v: json.loads(v.decode('ascii')),
-    key_deserializer=lambda v: v.decode('ascii'),
-    api_version = (0, 10, 1)
+admin = KafkaAdminClient(
+    bootstrap_servers='localhost:29092',
+    client_id='admin',
 )
 
-print(comsumer.subscription())
+try:
+    topic = NewTopic(name='real-time-stock-data', num_partitions=3, replication_factor=1)
+    admin.create_topics([topic], timeout_ms=3000)
+except Exception as e:
+    print(e)
 
-for message in comsumer:
-    print(message)
+admin.close()
+
+print(topic.name)
+
+comsumer1 = KafkaConsumer(
+    topic.name,
+    client_id='real-time-stock-consumer-1',
+    group_id='real-time-stock-group',
+    bootstrap_servers='localhost:29092',
+    value_deserializer=lambda v: json.loads(v.decode('ascii')),
+    key_deserializer=lambda v: json.loads(v.decode('ascii')),
+)
+
+comsumer2 = KafkaConsumer(
+    topic.name,
+    client_id='real-time-stock-consumer-2',
+    group_id='real-time-stock-group',
+    bootstrap_servers='localhost:29092',
+    value_deserializer=lambda v: json.loads(v.decode('ascii')),
+    key_deserializer=lambda v: json.loads(v.decode('ascii')),
+)
+
+def print_message(consumer, cid):
+    for message in consumer:
+        print(message.partition, message.offset, message.key, message.value, cid)
+
+thread1 = threading.Thread(target=print_message, args=(comsumer1, 1, ))
+thread2 = threading.Thread(target=print_message, args=(comsumer2, 2, ))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
--- service/data_producer.py
+++ service/data_producer.py
@@ -3,22 +3,25 @@ from kafka.errors import KafkaError
 import json
 from scraper import Scraper
 
+topic = 'real-time-stock-data'
+
 producer = KafkaProducer(
     client_id='real-time-stock-producer-1',
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='localhost:29092',
     value_serializer=lambda v: json.dumps(v).encode('ascii'),
-    key_serializer=lambda v: v.encode('ascii'),
-    api_version = (0, 10, 1)
+    key_serializer=lambda v: json.dumps(v).encode('ascii'),
 )
 
 raw_data = {'price': 100, 'name': 'AMZN'}
-
 for i in range(10):
     print("before")
-    producer.send('real-time-stock', key='AMZN', value=raw_data)
-    producer.flush()
+    producer.send(topic, key={'id': 0}, value=raw_data)
+    producer.send(topic, key={'id': 1}, value=raw_data)
+    producer.send(topic, key={'id': 2}, value=raw_data)
+    producer.send(topic, key={'id': 3}, value=raw_data)
     print("sent")
 
+producer.flush()
 # scraper_AMZN = Scraper('AMZN')
 # scraper_GOOG = Scraper('GOOG')
 # scraper_AAPL = Scraper('AAPL')
