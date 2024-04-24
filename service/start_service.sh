9f6e47cd2e0cd71323790f77c48c6ea8dd69e4ca
Shane <143796207+Shane0821@users.noreply.github.com>
2024-04-24 22:09:36 +0800

    merge docker compose; upd sh

--- service/start_service.sh
+++ service/start_service.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo docker compose -f ./docker-compose/zk-single-kafka-single.yml up -d
-
-sudo docker compose -f ./docker-compose/spark-master-worker.yml up -d
+sudo docker compose -f ./docker-compose.yml up -d
--- service/stop_service.sh
+++ service/stop_service.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo docker compose -f ./docker-compose/spark-master-worker.yml down
-
-sudo docker compose -f ./docker-compose/zk-single-kafka-single.yml down
+sudo docker compose -f ./docker-compose.yml down
 
