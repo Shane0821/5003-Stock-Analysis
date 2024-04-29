a5f4ce442a57ff365a56501c28764c284bdd880f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-04-29 16:07:29 +0800

    add topic

--- service/register_topic.py
+++ service/register_topic.py
@@ -8,6 +8,9 @@ admin = KafkaAdminClient(
 try:
     topic = NewTopic(name='real-time-stock-data', num_partitions=3, replication_factor=1)
     admin.create_topics([topic], timeout_ms=3000)
+
+    topic = NewTopic(name='real-time-stock-data-processed', num_partitions=3, replication_factor=1)
+    admin.create_topics([topic], timeout_ms=3000)
 except Exception as e:
     print(e)
 
