61a5e4079bb1325ecef1a7fc1214f50cba7d571f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-05-06 23:58:16 +0800

    add streaming processor service

--- service/build_images.sh
+++ service/build_images.sh
@@ -3,3 +3,5 @@
 sudo docker build --network host -f ./dockerfile/register_dockerfile -t topic-register .
 
 sudo docker build --network host -f ./dockerfile/producer_dockerfile -t data-producer .
+
+sudo docker build --network host -f ./dockerfile/processor_dockerfile -t streaming-processor .
