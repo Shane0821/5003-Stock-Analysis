9234692a58d385bfd4d9648d3b9871c87d4e7383
Shane <143796207+Shane0821@users.noreply.github.com>
2024-05-06 20:29:20 +0800

    upd build script

--- service/build_images.sh
+++ service/build_images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo docker build --network host -f ./dockerfile/register_dockerfile -t topic-register .
+
+sudo docker build --network host -f ./dockerfile/producer_dockerfile -t data-producer .
