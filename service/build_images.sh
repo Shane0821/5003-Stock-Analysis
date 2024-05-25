61c3f6c93e08c096d00660b36fc1612d1a4cad83
Shane <143796207+Shane0821@users.noreply.github.com>
2024-05-25 19:32:44 +0800

    add build

--- service/build_images.sh
+++ service/build_images.sh
@@ -5,3 +5,18 @@ sudo docker build --network host -f ./dockerfile/register_dockerfile -t topic-re
 sudo docker build --network host -f ./dockerfile/producer_dockerfile -t data-producer .
 
 sudo docker build --network host -f ./dockerfile/processor_dockerfile -t streaming-processor .
+
+# TODO: use s3 and remove this
+sudo docker build --network host -f ./dockerfile/k8s_spark_dockerfile -t k8s-spark .
+
+# sudo docker tag topic-register:latest shane233/topic-register:latest
+# sudo docker tag data-producer:latest shane233/data-producer:latest
+# sudo docker tag reddit-producer:latest shane233/reddit-producer:latest
+# sudo docker tag streaming-processor:latest shane233/streaming-processor:latest
+# sudo docker tag k8s-spark:latest shane233/k8s-spark:latest
+# sudo docker push shane233/topic-register:latest
+# sudo docker push shane233/data-producer:latest
+# sudo docker push shane233/reddit-producer:latest
+# sudo docker push shane233/streaming-processor:latest
+# sudo docker push shane233/k8s-spark:latest
+# sudo docker rmi shane233/topic-register:latest shane233/data-producer:latest shane233/streaming-processor:latest shane233/k8s-spark:latest shane233/reddit-producer:latest
