d6b8d38d9295bb527c6b5b62daaafe70c5ee1dc0
Shaun1280 <jpanau@connect.ust.hk>
2024-05-27 15:01:06 +0800

    Update kafka_websocket.py

--- service/code/kafka_websocket.py
+++ service/code/kafka_websocket.py
@@ -12,29 +12,37 @@ class KafkaWebSocketServer:
     async def consume_kafka(self, websocket):
         stock_data_consumer = AIOKafkaConsumer(
             self.kafka_topics[0],
-            bootstrap_servers=self.kafka_bootstrap_servers,
-            loop=asyncio.get_event_loop()
+            bootstrap_servers=self.kafka_bootstrap_servers
         )
         signal_consumer = AIOKafkaConsumer(
             self.kafka_topics[1],
-            bootstrap_servers=self.kafka_bootstrap_servers,
-            loop=asyncio.get_event_loop()
+            bootstrap_servers=self.kafka_bootstrap_servers
         )
         await stock_data_consumer.start()
         print("stock data consumer started")
         await signal_consumer.start()
         print("signal consumer started")
 
-        try:
+        async def consume_stock_data():
             async for msg in stock_data_consumer:
                 print(f"Consumed stock data message: {msg.value.decode('utf-8')}")
                 await websocket.send(msg.value.decode('utf-8'))
-            
+
+        async def consume_signal():
             async for msg in signal_consumer:
                 print(f"Consumed signal message: {msg.value.decode('utf-8')}")
                 await websocket.send(msg.value.decode('utf-8'))
+
+        try:
+            await asyncio.gather(
+                consume_stock_data(),
+                consume_signal()
+            )
+        except Exception as e:
+            print(f"An error occurred: {e}")
         finally:
             await stock_data_consumer.stop()
+            await signal_consumer.stop()
 
     async def handle_websocket(self, websocket, path):
         await self.consume_kafka(websocket)
@@ -44,9 +52,9 @@ class KafkaWebSocketServer:
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
-kafka_bootstrap_servers = 'kafka:9092'
+kafka_bootstrap_servers = '172.16.0.3:9092'
 kafka_topics = ['real-time-stock-data-processed', 'signal']
-websocket_host = 'localhost'
+websocket_host = '0.0.0.0'
 websocket_port = 8766
 
 server = KafkaWebSocketServer(
@@ -54,4 +62,5 @@ server = KafkaWebSocketServer(
     kafka_topics=kafka_topics,
     websocket_host=websocket_host,
     websocket_port=websocket_port)
+
 server.start()
