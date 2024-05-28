08c58f80e636c5cc47516c5f77524bf94feeb5a9
Shane <143796207+Shane0821@users.noreply.github.com>
2024-05-28 11:41:46 +0800

    upd kafka server hostname

--- service/code/kafka_websocket.py
+++ service/code/kafka_websocket.py
@@ -52,7 +52,7 @@ class KafkaWebSocketServer:
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
-kafka_bootstrap_servers = '172.16.0.3:9092'
+kafka_bootstrap_servers = 'kafka:9092'
 kafka_topics = ['real-time-stock-data-processed', 'signal-rsi-mac']
 websocket_host = '0.0.0.0'
 websocket_port = 8766
