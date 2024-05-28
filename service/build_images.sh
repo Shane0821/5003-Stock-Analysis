e588a850ef5dbc8a93d319e24df0d67263905af1
Shane <143796207+Shane0821@users.noreply.github.com>
2024-05-28 19:24:02 +0800

    disable upload

--- service/build_images.sh
+++ service/build_images.sh
@@ -10,14 +10,14 @@ sudo docker build --network host -f ./dockerfile/websocket_dockerfile -t kafka-w
 
 sudo docker build --network host -f ./dockerfile/k8s_spark_dockerfile -t k8s-spark .
 
-sudo docker tag topic-register:latest shane233/topic-register:latest
-sudo docker tag data-producer:latest shane233/data-producer:latest
-sudo docker tag streaming-processor:latest shane233/streaming-processor:latest
-sudo docker tag k8s-spark:latest shane233/k8s-spark:latest
-sudo docker tag kafka-websocket:latest shane233/kafka-websocket:latest
-sudo docker push shane233/topic-register:latest
-sudo docker push shane233/data-producer:latest
-sudo docker push shane233/streaming-processor:latest
-sudo docker push shane233/k8s-spark:latest
-sudo docker push shane233/kafka-websocket:latest
-sudo docker rmi shane233/topic-register:latest shane233/data-producer:latest shane233/streaming-processor:latest shane233/k8s-spark:latest shane233/kafka-websocket:latest
+# sudo docker tag topic-register:latest shane233/topic-register:latest
+# sudo docker tag data-producer:latest shane233/data-producer:latest
+# sudo docker tag streaming-processor:latest shane233/streaming-processor:latest
+# sudo docker tag k8s-spark:latest shane233/k8s-spark:latest
+# sudo docker tag kafka-websocket:latest shane233/kafka-websocket:latest
+# sudo docker push shane233/topic-register:latest
+# sudo docker push shane233/data-producer:latest
+# sudo docker push shane233/streaming-processor:latest
+# sudo docker push shane233/k8s-spark:latest
+# sudo docker push shane233/kafka-websocket:latest
+# sudo docker rmi shane233/topic-register:latest shane233/data-producer:latest shane233/streaming-processor:latest shane233/k8s-spark:latest shane233/kafka-websocket:latest
